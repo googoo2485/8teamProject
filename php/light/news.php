df5a90e6d95deb5290385c08ae3c6238b42c90ca
gksqlc5421 <gksqlc5421@naver.com>
2021-02-09 05:04:35 +0900

    news, category, login end

--- php/light/news.php
+++ php/light/news.php
@@ -17,7 +17,9 @@
 	
 	<div class="page">
 		<header class="header">
-			<img src="../img/logo2.png" alt="logo">
+		<a href="/index.php" target="_blank">
+			<img src="../img/logo_news.png" alt="logo" class="news-logo">
+		</a>
 			<form class="search-form">
 				<input type="search">
 				<input type="submit" value="찾기">
@@ -43,46 +45,40 @@
 		<section class="primary">
 			<ul class="card-list">
 				<li class="card-item">
-						<img src="../img/logo2.png" alt="logo">
+				<a href="https://www.blockmedia.co.kr/archives/169339" target="_blank">
+						<img src="../img/cme.jpg" alt="logo">
+				</a>
 					<div class="card-desc">
-						Lorem ipsum 
-					</div>
+					CME 이더리움 선물, 내일부터 거래 개시 미국 시카고상품거래소(CME) 공식 웹사이트에 따르면, 미국 동부표준시 기준 8일 18시(한국시간 기준 9일 08시)부터 이더리움(ETH, 시총 2위) 선물 계약의 거래가 개시됨. 					</div>
 				</li>
 				<li class="card-item">
-					<figure class="card-image"></figure>
+				<img src="../img/bitcoin.jpg" alt="logo">
 					<div class="card-desc">
-						Lorem ipsum.
-					</div>
+					럭시 디지털 창업자 "BTC, 2022년까지 랠리 지속 전망" 갤럭시 디지털 창업자 마이크 노보그라츠가 한 팟캐스트에 출연해 대형 은행들이 비트코인 시류에 편승하면서 비트코인이 향후 22개월 간 랠리를 지속할 것이라고 내다봄. 					</div>
 				</li>
 				<li class="card-item">
-					<figure class="card-image"></figure>
+				<img src="../img/Dogecoin.jpg" alt="logo" class="doge">
 					<div class="card-desc">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit libero in ratione dicta nihil corporis illo odio, voluptatem a nam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit libero in ratione dicta nihil corporis illo odio, voluptatem a nam!
-					</div>
+					도지코인 시총 8위.. LTC 제쳐 일론 머스크, 스눕독 등 유명인을 중심으로 트위터에서 도지코인 밈이 확산하며 도지코인 시세가 급등, 시가총액 순위 8위까지 오름. 현재 DOGE는 코인마켓캡 기준 52.91% 오른 0.08135 달러에 거래. 				</li>
+				<li class="card-item">
+				<img src="../img/ether.jpg" alt="logo">
+					<div class="card-desc">
+					이더리움 코어 개발자가 2월 7일 밤 트위터를 통해 이더리움 가스 리밋(gas limit) 상향에 반대한다고 밝힘. 그는 "가스 리밋 상향은 지난 6년간 네트워크를 유지해온 4명에게 불리하다"고 덧붙임. 					</div>
 				</li>
 				<li class="card-item">
-					<figure class="card-image"></figure>
+				<img src="../img/swarm.png" alt="logo" class="swarm">
 					<div class="card-desc">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit libero in ratione dicta nihil corporis illo odio, voluptatem a nam!
-					</div>
+					Swarm, 600만 달러 투자 유치 체인뉴스에 따르면 이더리움 기반 탈중앙 스토리지 프로토콜 Swarm이 600만 달러 사모 투자를 유치했다고 전했다. Alphemy Capital, Bitscale Capital, DFG, KR1, HashKey, NGC Ventures, The LAO 등이 투자에 참여. 					</div>
 				</li>
 				<li class="card-item">
-					<figure class="card-image"></figure>
+				<img src="../img/time.jpg" alt="logo">
 					<div class="card-desc">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit libero in ratione dicta nihil corporis illo odio, voluptatem a nam!
-					</div>
+					8일부터 비수도권 지역의 카페와 식당, 노래방, 실내체육시설 등은 영업시간이 기존 밤 9시에서 10시까지로 한 시간 연장됨. 					</div>
 				</li>
 				<li class="card-item">
-					<figure class="card-image"></figure>
+				<img src="../img/home.jpg" alt="logo">
 					<div class="card-desc">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit libero in ratione dicta nihil corporis illo odio, voluptatem a nam!
-					</div>
-				</li>
-				<li class="card-item">
-					<figure class="card-image"></figure>
-					<div class="card-desc">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit libero in ratione dicta nihil corporis illo odio, voluptatem a nam!
-					</div>
+					국민의 절반 이상은 4일 발표한 문재인 정부의 25번째 부동산 대책이 집값 안정에 별 도움이 되지 않을 것으로 보는 것으로 조사됨. 					</div>
 				</li>
 			</ul>
 		</section>
@@ -90,57 +86,43 @@
 		<aside class="secondary secondary-a">
 			<ul class="info-list">
 				<li class="info-list-item">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil fugit numquam aspernatur excepturi, eos soluta praesentium maiores commodi minus accusantium?
-				</li>
+				CME 이더리움 선물, 내일부터 거래 개시
+미국 시카고상품거래소(CME) 공식 웹사이트에 따르면, 미국 동부표준시 기준 8일 18시(한국시간 기준 9일 08시)부터 이더리움(ETH, 시총 2위) 선물 계약의 거래가 개시됨.				
+				</li><br><br>
 				<li class="info-list-item">
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam tempora nisi unde corrupti dicta.
-				</li>
+				럭시 디지털 창업자 "BTC, 2022년까지 랠리 지속 전망"
+갤럭시 디지털 창업자 마이크 노보그라츠가 한 팟캐스트에 출연해 대형 은행들이 비트코인 시류에 편승하면서 비트코인이 향후 22개월 간 랠리를 지속할 것이라고 내다봄.				
+				</li><br><br>
 				<li class="info-list-item">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam consequatur alias, ducimus ea magnam nostrum repudiandae repellendus deleniti veniam? Assumenda expedita ad eum nihil!
-				</li>
-			</ul>
-			<ul class="user-list friend-list">
-				<li class="user-item friend-item">
-					<figure class="user-photo" style="background-image: url();"></figure>
-					<p class="user-name">
-
-					</p>
-				</li>
-				<li class="user-item friend-item">
-					<figure class="user-photo" style="background-image: url();"></figure>
-					<p class="user-name">
-
-					</p>
-				</li>
-				<li class="user-item friend-item">
-					<figure class="user-photo" style="background-image: url();"></figure>
-					<p class="user-name">
-
-					</p>
-				</li>
-			</ul>
+				도지코인 시총 8위.. LTC 제쳐
+일론 머스크, 스눕독 등 유명인을 중심으로 트위터에서 도지코인 밈이 확산하며 도지코인 시세가 급등, 시가총액 순위 8위까지 오름. 현재 DOGE는 코인마켓캡 기준 52.91% 오른 0.08135 달러에 거래.
+				</li><br><br>
+				<li class="info-list-item">
+				이더리움 코어 개발자가 2월 7일 밤 트위터를 통해 이더리움 가스 리밋(gas limit) 상향에 반대한다고 밝힘. 그는 "가스 리밋 상향은 지난 6년간 네트워크를 유지해온 4명에게 불리하다"고 덧붙임.
+				</li><br><br>
+				<li class="info-list-item">
+				Swarm, 600만 달러 투자 유치
+체인뉴스에 따르면 이더리움 기반 탈중앙 스토리지 프로토콜 Swarm이 600만 달러 사모 투자를 유치했다고 전했다. Alphemy Capital, Bitscale Capital, DFG, KR1, HashKey, NGC Ventures, The LAO 등이 투자에 참여.			</ul>
+		
 		</aside>
 
 		<aside class="secondary secondary-b">
-			<ul class="user-list message-list">
-				<li class="user-item message-item">
-					<figure class="user-photo" style="background-image: url();"></figure>
-					<p class="message-content">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nobis, nisi numquam harum voluptates vel corrupti dolorem id, dicta eveniet similique architecto et, exercitationem quaerat alias ratione. Dicta, beatae, aspernatur, sit commodi quis illo non aut repellendus veritatis at ab.
-					</p>
-				</li>
-				<li class="user-item message-item">
-					<figure class="user-photo" style="background-image: url();"></figure>
-					<p class="message-content">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nobis, nisi numquam harum voluptates vel corrupti dolorem id, dicta eveniet similique architecto et, exercitationem quaerat alias ratione.
-					</p>
-				</li>
-				<li class="user-item message-item">
-					<figure class="user-photo" style="background-image: url();"></figure>
-					<p class="message-content">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nobis, nisi numquam harum voluptates vel corrupti dolorem id.
-					</p>
-				</li>
+		<ul class="info-list">
+				<li class="info-list-item">
+				8일부터 비수도권 지역의 카페와 식당, 노래방, 실내체육시설 등은 영업시간이 기존 밤 9시에서 10시까지로 한 시간 연장됨.
+				</li><br><br>
+				<li class="info-list-item">
+				국민의 절반 이상은 4일 발표한 문재인 정부의 25번째 부동산 대책이 집값 안정에 별 도움이 되지 않을 것으로 보는 것으로 조사됨.
+				</li><br><br>
+				<li class="info-list-item">
+				월요일은 전국이 대체로 맑은 가운데 기온이 큰 폭으로 떨어져 쌀쌀할 것으로 전망. 북서쪽에서 찬 공기가 유입되면서 경기 북부, 강원 내륙·산지의 아침 기온이 -10도 이하, 그 밖의 중부지방과 남부 내륙은 -5도 이하로 떨어짐.
+				</li><br><br>
+				<li class="info-list-item">
+				국내 코로나19 변이 확진자는 누적 51명으로 18개국에서 들어온 것으로 확인됨. 바이러스의 유입 추정 국가는 총 18개국.				
+				</li><br><br>
+				<li class="info-list-item">
+				인도 북부 히말라야산맥에서 거대한 눈사태가 일어나면서 마치 쓰나미 같은 급류가 하류지역을 휩쓸음. 마을 주민과 수력발전소 건설 현장의 노동자 등 2백여 명이 숨지거나 실종되는 참변.				
+				</li><br><br>
 			</ul>
 		</aside>
 		<!-- </div> -->
