9928491f5780413ce789b399cdfb9ad0fe633ccd
googoo2485 <googoo2485@github.com>
2021-05-17 17:06:59 +0900

    no message

--- php/sg/idConnect.php
+++ php/sg/idConnect.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-$conn = mysqli_connect('localhost','sanggoo','chiken99!','sanggoo');
+$conn = mysqli_connect('localhost','tkdrn2485','chiken99!!','tkdrn2485');
 
 //$result = mysqli_query($conn,$sql);
 //$fetch = mysqli_fetch_array($result);
